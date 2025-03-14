4adeb6ed2d4159d226032fae244a41d767ba025c
Cbrunig <cbbrunig@gmail.com>
2025-03-14 00:44:09 -0500

    cleaned up code and fixed issue with incorrect verification of password

--- password-validator.js
+++ password-validator.js
@@ -1,42 +1,30 @@
 const readline = require('readline-sync');
 
-let password = readline.question("Please enter a new password. Your password must be at least 8 characters long and contain:\nOne uppercase letter\nOne number\nNew Password: ");
-
 let uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numbers = "1234567890";
-let hasUppercase = false;
-let hasNumber = false;
 let isValid = false;
 
 do {
-    while (password.length < 8) { //check length
-        password = readline.question("Your password must be at least 8 characters long, please try again.\nNew Password: ");
-    }
-
-    while (!hasUppercase) {
-        for (let letter of password) { //check for uppercase letter
-            if (uppercaseLetters.includes(letter)) {
-                hasUppercase = true;
-                break;
-            }
+    
+    let password = readline.question("Please enter a new password. Your password must be at least 8 characters long and contain:\nOne uppercase letter\nOne number\nNew Password: ");
+    let hasUppercase = false;
+    let hasNumber = false;
+    
+    for (let letter of password) { //check for uppercase letter
+        if (uppercaseLetters.includes(letter)) {
+            hasUppercase = true;
         }
-        if (!hasUppercase)
-            password = readline.question("Your password must include at least one uppercase letter, please try again.\nNew Password: ");
-    }
-
-    while (!hasNumber) {
-        for (let letter of password) { //check for a number
-            if (numbers.includes(letter)) {
-                hasNumber = true;
-                break;
-            }
+        if (numbers.includes(letter)) {
+            hasNumber = true;
         }
-        if (!hasNumber)
-            password = readline.question("Your password must include at least one number, please try again.\nNew Password ");
     }
-    if (hasUppercase && hasNumber && password.length > 8) //checking all conditions
+  
+    if (hasUppercase && hasNumber && (password.length > 7)) //checking all conditions
         isValid = true;
+    else {
+       console.log("Invalid password. Make sure it is at least 8 characters long and contains at least one uppercase letter and one number.");
+    }
    
-} while (isValid = false);
+} while (!isValid);
 
 console.log("Your new password has been set.");
